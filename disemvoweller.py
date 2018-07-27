9ea44258582d4c7b02b5d8d5ed938905bc9a7af7
thegrassisstillgreen <andygrass92@gmail.com>
2018-07-27 14:43:19 -0400

    Added basic comments

--- disemvoweller.py
+++ disemvoweller.py
@@ -1,4 +1,4 @@
-class Disemvoweller(object):
+class Disemvoweller:
 	
 	def __init__(self, unluckyString):
 		self.unluckyString = unluckyString
@@ -6,24 +6,24 @@ class Disemvoweller(object):
 	def removeVowels(self):
 		i = 0
 		vowels = ['a', 'e', 'i', 'o', 'u', ' ']
-		self.weepers = []
+		self.weepers = [] #list that holds the removed vowels
 		
 		while i < len(self.unluckyString):
 			if self.unluckyString[i] in vowels:
 				if self.unluckyString[i] != ' ':
-					self.weepers.append(self.unluckyString[i])
-				self.unluckyString = self.unluckyString[:i] + self.unluckyString[i+1:]
+					self.weepers.append(self.unluckyString[i]) #add non-whitespace unwanted chars
+				self.unluckyString = self.unluckyString[:i] + self.unluckyString[i+1:] #string without unwanted char
 			else:
 				i += 1
 
 	def printDisemvowelled(self):
 		print(self.unluckyString, end=" ")
-		print(''.join(str(losers) for losers in self.weepers))
+		print(''.join(str(losers) for losers in self.weepers)) #output vowels removed with no whitespace 
 
 def runDisemvoweller():
 	unluckyString = input("Enter string to be disemvowelled: ")
 
-	if unluckyString == "quit":
+	if unluckyString == "quit": #if quit entered as string, exit program
 		exit()
 	
 	devStr = Disemvoweller(unluckyString)
